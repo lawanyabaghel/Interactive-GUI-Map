05dbc5207eeb3733824161169e5a1bed80110dc2
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-11-26 14:29:02 +0530

    Update LandQuakeMarker.java

--- src/module4/LandQuakeMarker.java
+++ src/module4/LandQuakeMarker.java
@@ -3,36 +3,17 @@ package module4;
 import de.fhpotsdam.unfolding.data.PointFeature;
 import processing.core.PGraphics;
 
-/** Implements a visual marker for land earthquakes on an earthquake map
- * 
- * @author UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
- *
- */
 public class LandQuakeMarker extends EarthquakeMarker {
 	
 	
 	public LandQuakeMarker(PointFeature quake) {
-		
-		// calling EarthquakeMarker constructor
 		super(quake);
-		
-		// setting field in earthquake marker
 		isOnLand = true;
 	}
 
-
-	@Override
+        @Override
 	public void drawEarthquake(PGraphics pg, float x, float y) {
 		// Draw a centered circle for land quakes
-		// DO NOT set the fill color here.  That will be set in the EarthquakeMarker
-		// class to indicate the depth of the earthquake.
-		// Simply draw a centered circle.
-		
-		// HINT: Notice the radius variable in the EarthquakeMarker class
-		// and how it is set in the EarthquakeMarker constructor
-		
-		// TODO: Implement this method
 		pg.ellipse(x, y, radius, radius);		
 	}
 	
@@ -41,8 +22,4 @@ public class LandQuakeMarker extends EarthquakeMarker {
 	public String getCountry() {
 		return (String) getProperty("country");
 	}
-
-
-
-		
 }
