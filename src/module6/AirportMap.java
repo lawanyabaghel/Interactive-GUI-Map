25143b49f90f77f18563cf813412b22012e254eb
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-11-27 14:14:14 +0530

    Update AirportMap.java

--- src/module6/AirportMap.java
+++ src/module6/AirportMap.java
@@ -15,12 +15,6 @@ import de.fhpotsdam.unfolding.geo.Location;
 import parsing.ParseFeed;
 import processing.core.PApplet;
 
-/** An applet that shows airports (and routes)
- * on a world map.  
- * @author Adam Setters and the UC San Diego Intermediate Software Development
- * MOOC team
- *
- */
 public class AirportMap extends PApplet {
 	
 	UnfoldingMap map;
@@ -28,28 +22,21 @@ public class AirportMap extends PApplet {
 	List<Marker> routeList;
 	
 	public void setup() {
-		// setting up PAppler
 		size(800,600, OPENGL);
 		
-		// setting up map and default events
 		map = new UnfoldingMap(this, 50, 50, 750, 550);
 		MapUtils.createDefaultEventDispatcher(this, map);
 		
-		// get features from airport data
 		List<PointFeature> features = ParseFeed.parseAirports(this, "airports.dat");
-		
-		// list for markers, hashmap for quicker access when matching with routes
+
 		airportList = new ArrayList<Marker>();
 		HashMap<Integer, Location> airports = new HashMap<Integer, Location>();
 		
-		// create markers from features
 		for(PointFeature feature : features) {
 			AirportMarker m = new AirportMarker(feature);
 	
 			m.setRadius(5);
 			airportList.add(m);
-			
-			// put airport in hashmap with OpenFlights unique id for key
 			airports.put(Integer.parseInt(feature.getId()), feature.getLocation());
 		
 		}
@@ -73,14 +60,8 @@ public class AirportMap extends PApplet {
 			SimpleLinesMarker sl = new SimpleLinesMarker(route.getLocations(), route.getProperties());
 		
 			System.out.println(sl.getProperties());
-			
-			//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
 			routeList.add(sl);
 		}
-		
-		
-		
-		//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
 		map.addMarkers(routeList);
 		
 		map.addMarkers(airportList);
@@ -90,7 +71,6 @@ public class AirportMap extends PApplet {
 	public void draw() {
 		background(0);
 		map.draw();
-		
 	}
 	
 
