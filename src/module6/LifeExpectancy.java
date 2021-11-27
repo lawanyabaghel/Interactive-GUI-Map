c23196d45fe5243d7f323baf349a2ffbfaa3217e
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-11-27 14:29:18 +0530

    Update LifeExpectancy.java

--- src/module6/LifeExpectancy.java
+++ src/module6/LifeExpectancy.java
@@ -18,10 +18,6 @@ import de.fhpotsdam.unfolding.marker.Marker;
 
 /**
  * Visualizes life expectancy in different countries. 
- * 
- * It loads the country shapes from a GeoJSON file via a data reader, and loads the population density values from
- * another CSV file (provided by the World Bank). The data value is encoded to transparency via a simplistic linear
- * mapping.
  */
 public class LifeExpectancy extends PApplet {
 
@@ -45,26 +41,21 @@ public class LifeExpectancy extends PApplet {
 		map.addMarkers(countryMarkers);
 		System.out.println(countryMarkers.get(0).getId());
 		
-		// Country markers are shaded according to life expectancy (only once)
+		// Country markers are shaded according to life expectancy
 		shadeCountries();
 	}
 
 	public void draw() {
-		// Draw map tiles and country markers
 		map.draw();
 	}
-
-	//Helper method to color each country based on life expectancy
 	//Red-orange indicates low (near 40)
 	//Blue indicates high (near 100)
 	private void shadeCountries() {
 		for (Marker marker : countryMarkers) {
-			// Find data for country of the current marker
 			String countryId = marker.getId();
 			System.out.println(lifeExpMap.containsKey(countryId));
 			if (lifeExpMap.containsKey(countryId)) {
 				float lifeExp = lifeExpMap.get(countryId);
-				// Encode value as brightness (values range: 40-90)
 				int colorLevel = (int) map(lifeExp, 40, 90, 10, 255);
 				marker.setColor(color(255-colorLevel, 100, colorLevel));
 			}
