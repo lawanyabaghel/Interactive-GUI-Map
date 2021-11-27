dd1db1acd607e4cc9db9f367762f718dc668f0bf
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-11-27 14:17:29 +0530

    Update CityMarker.java

--- src/module6/CityMarker.java
+++ src/module6/CityMarker.java
@@ -6,14 +6,9 @@ import de.fhpotsdam.unfolding.geo.Location;
 import processing.core.PConstants;
 import processing.core.PGraphics;
 
-/** Implements a visual marker for cities on an earthquake map
- * 
- * @author UC San Diego Intermediate Software Development MOOC team
- * 
- */
 public class CityMarker extends CommonMarker {
 	
-	public static int TRI_SIZE = 5;  // The size of the triangle marker
+	public static int TRI_SIZE = 5;  
 	
 	public CityMarker(Location location) {
 		super(location);
@@ -22,35 +17,16 @@ public class CityMarker extends CommonMarker {
 	
 	public CityMarker(Feature city) {
 		super(((PointFeature)city).getLocation(), city.getProperties());
-		// Cities have properties: "name" (city name), "country" (country name)
-		// and "population" (population, in millions)
 	}
 	
 	
-	// pg is the graphics object on which you call the graphics
-	// methods.  e.g. pg.fill(255, 0, 0) will set the color to red
-	// x and y are the center of the object to draw. 
-	// They will be used to calculate the coordinates to pass
-	// into any shape drawing methods.  
-	// e.g. pg.rect(x, y, 10, 10) will draw a 10x10 square
-	// whose upper left corner is at position x, y
-	/**
-	 * Implementation of method to draw marker on the map.
-	 */
 	public void drawMarker(PGraphics pg, float x, float y) {
-		//System.out.println("Drawing a city");
-		// Save previous drawing style
 		pg.pushStyle();
-		
-		// IMPLEMENT: drawing triangle for each city
 		pg.fill(150, 30, 30);
 		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
-		
-		// Restore previous drawing style
 		pg.popStyle();
 	}
 	
-	/** Show the title of the city if this marker is selected */
 	public void showTitle(PGraphics pg, float x, float y)
 	{
 		String name = getCity() + " " + getCountry() + " ";
