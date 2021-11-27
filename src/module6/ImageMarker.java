38e778a937e38dd98b32633fbe8b09e011b4ec1a
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2021-11-27 14:27:07 +0530

    Update ImageMarker.java

--- src/module6/ImageMarker.java
+++ src/module6/ImageMarker.java
@@ -7,9 +7,6 @@ import processing.core.PImage;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.AbstractMarker;
 
-/**
- * This marker displays an image at its location.
- */
 public class ImageMarker extends AbstractMarker {
 
 	PImage img;
@@ -23,7 +20,6 @@ public class ImageMarker extends AbstractMarker {
 	public void draw(PGraphics pg, float x, float y) {
 		pg.pushStyle();
 		pg.imageMode(PConstants.CORNER);
-		// The image is drawn in object coordinates, i.e. the marker's origin (0,0) is at its geo-location.
 		pg.image(img, x - 11, y - 37);
 		pg.popStyle();
 	}
