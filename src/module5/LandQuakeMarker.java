d890bc9586c9ae8281e62057d8da190d80361821
lawanyabaghel <92780143+lawanyabaghel@users.noreply.github.com>
2022-01-05 19:30:58 +0530

    Update LandQuakeMarker.java

--- src/module5/LandQuakeMarker.java
+++ src/module5/LandQuakeMarker.java
@@ -4,21 +4,19 @@ import de.fhpotsdam.unfolding.data.PointFeature;
 import processing.core.PGraphics;
 public class LandQuakeMarker extends EarthquakeMarker {
 	
-	
 	public LandQuakeMarker(PointFeature quake) {
+		// calling EarthquakeMarker constructor
 		super(quake);
 		isOnLand = true;
 	}
-
+        // Draw the earthquake as an ellipse
 	@Override
 	public void drawEarthquake(PGraphics pg, float x, float y) {
 		pg.ellipse(x, y, 2*radius, 2*radius);
 		
 	}
-	
+	// Get the country the earthquake is in
 	public String getCountry() {
 		return (String) getProperty("country");
 	}
-
-		
 }
